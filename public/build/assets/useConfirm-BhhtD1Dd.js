cf939c610261b232488ca607b7ce948374b6b591
Rotten Orange <younista666@gmail.com>
2025-12-28 11:43:17 +0700

    Build frontend assets - 2025-12-28 11:43:17

--- public/build/assets/useConfirm-BhhtD1Dd.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./FlashToaster-AYY5HN4c.js";function e(){return{show:r=>o.show(r),confirm:(r,n={})=>o.show({message:r,...n})}}export{e as u};
---
+++ public/build/assets/useConfirm-DoHt9LrG.js
@@ -0,0 +1,1 @@
+import{c as o}from"./FlashToaster-BAEPXES7.js";function e(){return{show:r=>o.show(r),confirm:(r,n={})=>o.show({message:r,...n})}}export{e as u};
